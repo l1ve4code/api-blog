49a8181caff330e67964f494d97ae671ca036693
l1ve4code <steven.marelly@gmail.com>
2022-05-25 16:03:59 +0300

    Added user id JWT support

--- src/main/java/ru/live4code/blog/api/controller/AdminRestControllerV1.java
+++ src/main/java/ru/live4code/blog/api/controller/AdminRestControllerV1.java
@@ -3,10 +3,12 @@ package ru.live4code.blog.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.live4code.blog.api.dto.CommentDTO;
 import ru.live4code.blog.api.dto.NewsDTO;
 import ru.live4code.blog.api.dto.RoleDTO;
+import ru.live4code.blog.api.security.jwt.JwtUser;
 import ru.live4code.blog.api.service.CommentService;
 import ru.live4code.blog.api.service.NewsService;
 import ru.live4code.blog.api.service.RoleService;
@@ -34,9 +36,10 @@ public class AdminRestControllerV1 {
     }
 
     @PostMapping("/news")
-    public ResponseEntity addNews(@RequestBody NewsDTO newsDTO) {
+    public ResponseEntity addNews(Authentication authentication, @RequestBody NewsDTO newsDTO) {
+        long user_id = ((JwtUser) authentication.getPrincipal()).getId();
         News news = NewsDTO.to(newsDTO);
-        NewsDTO isCreated = newsService.create(news, 6);
+        NewsDTO isCreated = newsService.create(news, user_id);
         if (isCreated == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         return ResponseEntity.ok(isCreated);
     }
--- src/main/java/ru/live4code/blog/api/controller/UserRestControllerV1.java
+++ src/main/java/ru/live4code/blog/api/controller/UserRestControllerV1.java
@@ -3,9 +3,11 @@ package ru.live4code.blog.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.live4code.blog.api.dto.CommentDTO;
 import ru.live4code.blog.api.dto.LikeDTO;
+import ru.live4code.blog.api.security.jwt.JwtUser;
 import ru.live4code.blog.api.service.CommentService;
 import ru.live4code.blog.api.service.LikeService;
 
@@ -21,8 +23,9 @@ public class UserRestControllerV1 {
 
     // LIKES
     @PostMapping("/like/{id}")
-    public ResponseEntity setLiked(@PathVariable("id") long id){
-        LikeDTO like = likeService.create(6, id);
+    public ResponseEntity setLiked(Authentication authentication, @PathVariable("id") long id){
+        long user_id = ((JwtUser) authentication.getPrincipal()).getId();
+        LikeDTO like = likeService.create(user_id, id);
         if (like == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         return ResponseEntity.ok(like);
     }
